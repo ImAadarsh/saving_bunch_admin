9da1fa2eeafeb67e2e546c0692f0b67bdacc47cf
Aadarsh Gupta <67497622+ImAadarsh@users.noreply.github.com>
2024-01-22 14:25:06 +0530

    Update AddCoupanModal.jsx

--- src/pages/Modals/AddCoupanModal.jsx
+++ src/pages/Modals/AddCoupanModal.jsx
@@ -158,7 +158,7 @@ const AddCoupanModal = (props) => {
                     </div>
                     <div>
                       <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an category</label>
-                      <select id="category" name="category" value={value.category} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                      <select id="category" name="category" value={value.category._id} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option selected>Choose a category</option>
                         {category.map((e,index)=>{
                           return <option key={index} value={e._id}>{e.title}</option>
@@ -167,7 +167,7 @@ const AddCoupanModal = (props) => {
                     </div>
                     <div>
                       <label htmlFor="store" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an store</label>
-                      <select id="store" name="store" value={value.store} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                      <select id="store" name="store" value={value.store._id} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option selected>Choose a store</option>
                         {stores.map((e,index)=>{
                           return <option key={index} value={e._id}>{e.title}</option>
